a19231115d806aaa5c442628cf8eea73eebfaeec
G.F.Pajarinen <NossaCasa@GFMacBook-Pro.local>
2020-07-14 16:29:02 +0300

    ex04 m2 ok

--- Modulo II/Exercicio04.java
+++ Modulo II/Exercicio04.java
@@ -1,2 +1,27 @@
-/**Faça um programa que receba várias idades, calcule e mostre a 
- * média das idades digitadas. Finalize digitando idade igual a zero. */
+/**
+ * Faça um programa que receba várias idades, calcule e mostre a 
+ * média das idades digitadas. 
+ * Finalize digitando idade igual a zero. 
+ * */
+
+ public class Exercicio04{
+
+    public static void main(String[] args) {
+        
+        int idade, soma=0, cont=1;
+        double media;
+
+        do{
+            System.out.println("Digite a idade: ");
+            idade = Integer.parseInt(System.console().readLine());
+
+            soma += idade;
+            cont++;
+        }
+        while(idade != 0);
+
+        media = (double)  soma / cont;
+
+        System.out.printf("A média de todas as idades é: %.1f\n", media);
+    }
+ }
