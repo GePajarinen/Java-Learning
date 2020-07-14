5f2c045bf65018bed9a47e2726d99ad2170883d9
G.F.Pajarinen <NossaCasa@GFMacBook-Pro.local>
2020-07-14 15:28:20 +0300

    ex1 m2 ok

--- Modulo II/Exercicio01.java
+++ Modulo II/Exercicio01.java
@@ -1,3 +1,20 @@
 /**
  * Faça um programa que calcule a soma de dez números quaisquer fornecidos pelo usuário
  */
+
+ public class Exercicio01{
+     public static void main (String[] args){
+
+        int numero, soma=0;
+
+        for (int i=1; i<=10 ; i++ ){
+            
+            System.out.printf("Digite o %d° número: \n>> ", i);
+            numero = Integer.parseInt(System.console().readLine());
+            soma += numero;
+        }
+
+        System.out.printf(" A soma dos números é: %d\n", soma);
+        
+     }
+ }
