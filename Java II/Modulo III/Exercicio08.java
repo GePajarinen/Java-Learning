8744cd399c07ea9356cabb402991b1354dbf2f8f
G.F.Pajarinen <NossaCasa@GFMacBook-Pro.local>
2020-07-15 17:18:49 +0300

    ex 08 md 3 ok

--- Java II/Modulo III/Exercicio08.java
+++ Java II/Modulo III/Exercicio08.java
@@ -1,11 +1,20 @@
 /**
  * EXERCÍCIO 08
- *  Faça um algoritmo e um programa, utilizando as estruturas repita e do..while
+ *  Faça um programa, utilizando as estruturas repita e do..while
  *  que repita a leitura de uma senha até que o usuário digite o número 1234. 
  * Após isso, mostrar a mensagem “Senha correta, acesso liberado.”
  */
 
- 
 public class Exercicio08 {
-    
+    public static void main(String[] args) {
+        int senha;
+
+        do{
+            System.out.println("Digite sua senha: ");
+            senha = Integer.parseInt(System.console().readLine());
+
+        } while(senha != 1234);
+
+        System.out.println("Senha correta, acesso liberado.");
+    }
 }
