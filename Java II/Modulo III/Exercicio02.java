8edaa21789995a1edf3d8d1987f6bd7feb0933ab
Gesiane Pajarinen <gesianef@hotmail.com>
2020-08-15 22:08:57 +0300

    Update Exercicio02.java

--- Java II/Modulo III/Exercicio02.java
+++ Java II/Modulo III/Exercicio02.java
@@ -1,28 +1,23 @@
-import java.util.Scanner; 
-import java.math;
-
 /**
- * EXERCÍCIO 02
- * O Detran deseja realizar uma vistoria geral nos veículos do estado. Para isso, elaborou 
- * a seguinte distribuição, a partir do último dígito do número na placa do automóvel:
- * 
- Placas que terminam com 0 devem realizar a vistoria em Janeiro do próximo ano
- Placas que terminam com 1 devem realizar a vistoria em Fevereiro do próximo ano
- Placas que terminam com 2 devem realizar a vistoria em Março do próximo ano
- Placas que terminam com 3 devem realizar a vistoria em Abril do próximo ano
- Placas que terminam com 4 devem realizar a vistoria em Maio do próximo ano
- Placas que terminam com 5 devem realizar a vistoria em Junho do próximo ano
- Placas que terminam com 6 devem realizar a vistoria em Setembro deste ano
- Placas que terminam com 7 devem realizar a vistoria em Outubro deste ano
- Placas que terminam com 8 devem realizar a vistoria em Novembro deste ano
- Placas que terminam com 9 devem realizar a vistoria em Dezembro deste ano
+* EXERCÍCIO 02
+O Detran deseja realizar uma vistoria geral nos veículos do estado. Para isso, elaborou a seguinte distribuição, a partir do último dígito do número na placa do automóvel:
 
-Escreva um algoritmo (utilizando escolha) e um programa em Java (utilizando switch) 
-que, a partir do número da placa de um veículo (um inteiro de 4 dígitos), mostrem 
-em qual mês ele deve realizar a vistoria. DICA: para obter o último dígito da placa 
-obtenha o resto a divisão dela por 10.
- */
+- Placas que terminam com 0 devem realizar a vistoria em Janeiro do próximo ano
+- Placas que terminam com 1 devem realizar a vistoria em Fevereiro do próximo ano
+- Placas que terminam com 2 devem realizar a vistoria em Março do próximo ano
+- Placas que terminam com 3 devem realizar a vistoria em Abril do próximo ano
+- Placas que terminam com 4 devem realizar a vistoria em Maio do próximo ano
+- Placas que terminam com 5 devem realizar a vistoria em Junho do próximo ano
+- Placas que terminam com 6 devem realizar a vistoria em Setembro deste ano
+- Placas que terminam com 7 devem realizar a vistoria em Outubro deste ano
+- Placas que terminam com 8 devem realizar a vistoria em Novembro deste ano
+- Placas que terminam com 9 devem realizar a vistoria em Dezembro deste ano
 
+Escreva um algoritmo (utilizando escolha) e um programa em Java (utilizando switch) que, a partir do número da placa de um veículo (um inteiro de 4 dígitos), mostrem em qual mês ele deve realizar a vistoria. DICA: para obter o último dígito da placa obtenha o resto a divisão dela por 10.
+*/
+
+import java.util.Scanner; 
+import java.lang.Math;
  
 public class Exercicio02 {
     public static void main (String[] args){
