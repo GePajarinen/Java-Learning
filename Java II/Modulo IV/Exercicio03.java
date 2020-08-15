cd1978213d26d46825ea6d76e6d6ee16f94daa41
GePajarinen <gesianef@hotmail.com>
2020-08-15 15:41:38 +0300

    exe 03 modulo4 java ii

--- Java II/Modulo IV/Exercicio03.java
+++ Java II/Modulo IV/Exercicio03.java
@@ -1,4 +1,5 @@
 /**
+ * EXERCÍCIO 03
  * Escreva um programa em Java que leia um string contendo um 
  * número em binário e o mostre em decimal na tela.
  */
--- Java II/Modulo IV/Exercicio04.java
+++ Java II/Modulo IV/Exercicio04.java
@@ -1,4 +1,5 @@
 /**
+ * EXERCÍCIO 04
  * Escreva um programa em Java que calcule as raízes de uma equação de 
  * segundo grau utilizando a fórmula de Báskara. A equação de segundo 
  * grau é uma equação no formato Ax2 + Bx + C. O usuário deve informar os 
