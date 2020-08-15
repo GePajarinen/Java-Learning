7cf42872cd9fe7463b513145a71f798a9426f050
GePajarinen <gesianef@hotmail.com>
2020-08-15 15:27:53 +0300

    exercicio 04 mudolo 4 javaII

--- Java II/Modulo IV/Exercicio04.java
+++ Java II/Modulo IV/Exercicio04.java
@@ -5,6 +5,33 @@
  * valores para A, B e C.
  */
 
+import java.lang.Math;
+
 public class Exercicio04 {
-    
+    public static void main (String[] args){
+            
+        double delta, x1, x2, A, B, C;
+
+        System.out.println("Digite um valor para A: \n");
+        A = Double.parseDouble(System.console().readLine());
+        System.out.println("Digite um valor para B: \n");
+        B = Double.parseDouble(System.console().readLine());
+        System.out.println("Digite um valor para C: \n");
+        C = Double.parseDouble(System.console().readLine());
+
+        delta = Math.pow(B,2) - (4*A*C);
+
+        if (delta < 0){ //equação não possui resultados reais;
+        System.out.println("A equação não possui resultados reais.\n");
+        }
+        if(delta == 0){//a equação possui apenas um resultado real 
+        x1 = ((-1*B)+ (Math.sqrt(delta)))/ (2*A);
+        System.out.format("x1 = x2 = %f%n", x1);
+        }
+        if (delta > 0){ // a equação possui dois resultados distintos reais.
+        x1 = ((-1*B)+ (Math.sqrt(delta)))/ (2*A);
+        x2 = ((-1*B)- (Math.sqrt(delta)))/ (2*A);
+        System.out.format("\nS = { %.2f, %.2f}\n", x1, x2);
+        }
+    }
 }
