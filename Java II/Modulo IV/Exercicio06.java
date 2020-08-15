54c3bb3662dc503c0ce4abff2755717f77728061
GePajarinen <gesianef@hotmail.com>
2020-08-15 12:14:21 +0300

    exer 06 modulo 4 java II

--- Java II/Modulo IV/Exercicio06.java
+++ Java II/Modulo IV/Exercicio06.java
@@ -3,6 +3,13 @@
  * ou seja, sorteie um número entre 1 e 6.
  */
 
+import java.lang.Math;
+
 public class Exercicio06 {
+    public static void main(String[] args) {
+        int num; 
     
+        num =  1 + (int)(Math.random()*6);
+        System.out.format("O número sorteado foi: %d%n", num);
+    }
 }
