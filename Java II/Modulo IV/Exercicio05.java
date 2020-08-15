6273f94a265cabec22a6484be325b625476727c1
GePajarinen <gesianef@hotmail.com>
2020-08-15 13:45:12 +0300

    BigDecimal, .setScale()

--- Java II/Modulo IV/Exercicio05.java
+++ Java II/Modulo IV/Exercicio05.java
@@ -5,6 +5,25 @@
  * casas decimais, o número seria arredondado para 1,14.
  */
 
+import java.math.*; 
+
 public class Exercicio05 {
-    
+  public static void main(String[] args) {
+    BigDecimal num;
+    int casas;
+
+    System.out.println("Digite o número real: \n");
+    //num = Float.parseFloat(System.console().readLine());
+    num = new BigDecimal(System.console().readLine());
+
+    System.out.println("\nPara quantas casas decimais deseja arrendodar? \n");
+    casas= Integer.parseInt(System.console().readLine());
+
+    BigDecimal resultado = num.setScale(casas, RoundingMode.FLOOR);
+    System.out.println(resultado);
+
+
+    //https://www.tutorialspoint.com/java/math/bigdecimal_setscale_rm_roundingmode.htm
+
+  }
 }
