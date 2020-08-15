0511aa1fc173ca649ad7d5d6fd660015cdfbd961
GePajarinen <gesianef@hotmail.com>
2020-08-15 16:01:42 +0300

    exer 03 modulo 4 java II

--- Java II/Modulo IV/Exercicio03.java
+++ Java II/Modulo IV/Exercicio03.java
@@ -3,7 +3,20 @@
  * Escreva um programa em Java que leia um string contendo um 
  * número em binário e o mostre em decimal na tela.
  */
+    
+import java.util.Scanner;
 
 public class Exercicio03 {
+
+  public static void main(String[] args) {
+  
+    System.out.print("Digite o número binário:\n ");
+
+    Scanner sc = new Scanner( System.in );
     
+    String num_binario = sc.nextLine();
+    
+    System.out.println("O número em Decimal é: "+ Integer.parseInt(num_binario,2));
+  }
+  
 }
