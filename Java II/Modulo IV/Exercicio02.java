66da2a3413add39091a81e47580d1d2fa36a20d8
Gesiane Pajarinen <gesianef@hotmail.com>
2020-08-16 10:16:06 +0300

    Update Exercicio02.java

--- Java II/Modulo IV/Exercicio02.java
+++ Java II/Modulo IV/Exercicio02.java
@@ -1,8 +1,18 @@
 /**
- * Escreva um programa em Java que mostre os números
- * os números de 0 a 100 e seus correspondentes em binário.
- */
+Escreva um programa em Java que mostre os números de 0 a 100 e seus correspondentes em binário.
+*/
 
 public class Exercicio02 {
-    
+
+  public static void main(String[] args) {
+    int i = 0;
+
+    while(i<=100){
+
+      System.out.format("O binário de %d é: %s%n", i, Integer.toBinaryString(i));
+      i++;
+
+    }
+
+  }
 }
