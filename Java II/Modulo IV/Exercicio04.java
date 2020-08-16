abb171c2b9ce46725a1d5c27729acb1f4f75720a
Gesiane Pajarinen <gesianef@hotmail.com>
2020-08-16 10:56:26 +0300

    Update Exercicio04.java

--- Java II/Modulo IV/Exercicio04.java
+++ Java II/Modulo IV/Exercicio04.java
@@ -1,38 +1,39 @@
 /**
- * EXERCÍCIO 04
- * Escreva um programa em Java que calcule as raízes de uma equação de 
- * segundo grau utilizando a fórmula de Báskara. A equação de segundo 
- * grau é uma equação no formato Ax2 + Bx + C. O usuário deve informar os 
- * valores para A, B e C.
- */
+* EXERCÍCIO 04
+Escreva um programa em Java que calcule as raízes de uma equação de segundo grau utilizando a fórmula de Báskara. A equação de segundo grau é uma equação no formato Ax2 + Bx + C. O usuário deve informar os valores para A, B e C.
+*/
 
 import java.lang.Math;
 
 public class Exercicio04 {
-    public static void main (String[] args){
+
+  public static void main (String[] args){
             
-        double delta, x1, x2, A, B, C;
+    double delta, x1, x2, A, B, C;
 
-        System.out.println("Digite um valor para A: \n");
-        A = Double.parseDouble(System.console().readLine());
-        System.out.println("Digite um valor para B: \n");
-        B = Double.parseDouble(System.console().readLine());
-        System.out.println("Digite um valor para C: \n");
-        C = Double.parseDouble(System.console().readLine());
+    System.out.println("\nDigite um valor para A:");
+    A = Double.parseDouble(System.console().readLine());
 
-        delta = Math.pow(B,2) - (4*A*C);
+    System.out.println("\nDigite um valor para B:");
+    B = Double.parseDouble(System.console().readLine());
 
-        if (delta < 0){ //equação não possui resultados reais;
-        System.out.println("A equação não possui resultados reais.\n");
-        }
-        if(delta == 0){//a equação possui apenas um resultado real 
-        x1 = ((-1*B)+ (Math.sqrt(delta)))/ (2*A);
-        System.out.format("x1 = x2 = %f%n", x1);
-        }
-        if (delta > 0){ // a equação possui dois resultados distintos reais.
-        x1 = ((-1*B)+ (Math.sqrt(delta)))/ (2*A);
-        x2 = ((-1*B)- (Math.sqrt(delta)))/ (2*A);
-        System.out.format("\nS = { %.2f, %.2f}\n", x1, x2);
-        }
+    System.out.println("\nDigite um valor para C:");
+    C = Double.parseDouble(System.console().readLine());
+
+    delta = Math.pow(B,2) - (4*A*C);
+
+    if (delta < 0){ //equação não possui resultados reais;
+      System.out.println("A equação não possui resultados reais.\n");
     }
+    if(delta == 0){//a equação possui apenas um resultado real 
+      x1 = ((-1*B)+ (Math.sqrt(delta)))/ (2*A);
+      System.out.format("x1 = x2 = %f%n", x1);
+    }
+    if (delta > 0){ // a equação possui dois resultados distintos reais.
+      x1 = ((-1*B)+ (Math.sqrt(delta)))/ (2*A);
+      x2 = ((-1*B)- (Math.sqrt(delta)))/ (2*A);
+      System.out.format("\nS = { %.2f, %.2f}\n", x1, x2);      
+    }
+
+  }
 }
