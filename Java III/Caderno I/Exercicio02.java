76bc3d85540e5b96a005e02784f4f83267cb5f0d
Gesiane Pajarinen <gesianef@hotmail.com>
2020-08-30 10:27:09 +0300

    Update Exercicio02.java

--- Java III/Caderno I/Exercicio02.java
+++ Java III/Caderno I/Exercicio02.java
@@ -1,6 +1,7 @@
 /*
-2. Faça um programa que receba o tipo de ação, ou seja, uma letra qualquer a ser comercializada na bolsa de valores, o preço de compra e o preço de venda de cada ação e que calcule e mostre:
- a quantidade de ações com lucro superior a R$ 1.000,00;
- a quantidade de ações com lucro inferior a R$ 200,00.
+EXERCÍCIO 02
+Faça um programa que receba o tipo de ação, ou seja, uma letra qualquer a ser comercializada na bolsa de valores, o preço de compra e o preço de venda de cada ação e que calcule e mostre:
+- a quantidade de ações com lucro superior a R$ 1.000,00;
+- a quantidade de ações com lucro inferior a R$ 200,00.
 Finalize com o tipo de ação ‘F’.
 */
